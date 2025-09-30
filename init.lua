97541495f3bac0f6d6dfb21b8e2224bb8c029c6d
Shaun10020 <35424159+Shaun10020@users.noreply.github.com>
2025-09-30 11:09:14 +0000

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,18 @@
-require('my_vim_config')
-require('my_lazy')
-require('my_theme')
-require('my_telescope')
+require('my_vim_config') -- Personal Edit
+require('my_lazy') -- Plugin Installation Manager
+require('my_theme') -- Theme
+require('my_telescope') -- Searching file explorer
 require('my_lsp-zero')
 require('my_dap')
 require('my_mason-lsp')
-require('my_oil')
-require('my_cmp')
+require('my_oil') -- Vim like feature to Create, Rename, and Delete Files
+require('my_cmp') -- Auto completion 
 require('my_gitsigns')
 require('my_toggleterm')
 require('my_harpoon')
 require('my_ibl')
-require('my_lualine')
+require('my_lualine') -- Give more info at the line bar
 require('my_noice')
-require('my_drop')
+require('my_drop') -- Computer Idle Animation
 -- require('my_precognition')
 -- require('my_treesitter')
