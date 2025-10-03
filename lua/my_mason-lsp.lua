b58dc61d4607cdeec505aa8b902f0867324e040f
Shaun10020 <35424159+Shaun10020@users.noreply.github.com>
2025-10-03 06:29:02 +0000

    Update my_mason-lsp.lua

--- lua/my_mason-lsp.lua
+++ lua/my_mason-lsp.lua
@@ -31,10 +31,5 @@ vim.keymap.set('n','<leader>mm',function() vim.cmd("Mason") end)
 -- vim.cmd("MasonInstall typescript-language-server") -- TypeScript
 
 require('mason-lspconfig').setup({})
-require('mason-lspconfig').setup_handlers({
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-})
 
 
